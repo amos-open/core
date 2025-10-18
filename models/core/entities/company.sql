ccf59916d3559546f63c9bf4d28326cf0d3efe1f
Amos HQ <benjamin@amos.tech>
2025-10-18 22:59:44 +0200

    Standardize column mappings across transformation layers

--- models/core/entities/company.sql
+++ models/core/entities/company.sql
@@ -6,21 +6,14 @@
   )
 }}
 
-WITH staging_company AS (
+WITH intermediate_company AS (
   SELECT * FROM {{ ref('amos_source_example', 'int_entities_company') }}
 ),
 
 validated_company AS (
   SELECT
-    id,
-    name,
-    website,
-    description,
-    currency,
-    industry_id,
-    created_at,
-    updated_at
-  FROM staging_company
+    {{ amos_source_example.alias_intermediate_columns('company') }}
+  FROM intermediate_company
   WHERE 1=1
     -- Entity base validation
     AND {{ validate_entity_base_fields('id', 'name') }}
@@ -29,12 +22,5 @@ validated_company AS (
 )
 
 SELECT
-  id,
-  name,
-  website,
-  description,
-  currency,
-  industry_id,
-  created_at,
-  updated_at
+  {{ amos_source_example.alias_intermediate_columns('company') }}
 FROM validated_company
